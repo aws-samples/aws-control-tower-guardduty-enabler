1707e7b5faddc6ba9bcb82b6bb9c23cc17e4a4b8
Chris Vowles <chris@scalefactory.com>
2022-05-12 16:21:29 +0100

    Simplify package script, no docker

--- src/package.sh
+++ src/package.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-# Builds a lambda package from a single Python 3 module with pip dependencies.
-# This is a modified version of the AWS packaging instructions:
-# https://docs.aws.amazon.com/lambda/latest/dg/lambda-python-how-to-create-deployment-package.html#python-package-dependencies
+# Builds a lambda package, a zip with a single Python file
 
 set -e
 set -x
@@ -9,16 +7,10 @@ set -x
 # https://stackoverflow.com/a/246128
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-rm -rf ${SCRIPT_DIRECTORY}/dist
-
-mkdir ${SCRIPT_DIRECTORY}/dist
-cp ${SCRIPT_DIRECTORY}/guardduty_enabler.py ${SCRIPT_DIRECTORY}/dist
-cp ${SCRIPT_DIRECTORY}/requirements.txt ${SCRIPT_DIRECTORY}/dist
-
-docker run -w /build/src/dist -v ${PWD}:/build -ti python:3.7 /usr/local/bin/pip3 install --target . -r requirements.txt
+pushd $SCRIPT_DIRECTORY > /dev/null
  
-pushd ${SCRIPT_DIRECTORY}/dist > /dev/null
+rm -f guardduty_enabler.zip > /dev/null 2>&1
 
-zip --grow  guardduty_enabler.zip *
- 
+zip guardduty_enabler.zip guardduty_enabler.py
+
 popd > /dev/null
